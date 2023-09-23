8341c36878e878183b0ec057f112cb10e920b534
Ruben Barros <atosc08@gmail.com>
2023-09-23 17:06:10 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export interface Env {
+	SHORT: KVNamespace;
+}
+
+export default {
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const url = new URL(request.url);
+		const short = url.pathname.split('/')[1];
+		if (short === '') {
+			return new Response('Not found', { status: 404 });
+		}
+		const redirect = await env.SHORT.get(short);
+		if (redirect === null) {
+			return new Response('Not found', { status: 404 });
+		}
+		return Response.redirect(redirect, 301);
+	},
+};
